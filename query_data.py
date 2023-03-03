d0f11739ab36c1e031a8e50364cf6a205de6a845
Hamel Husain <hamel.husain@gmail.com>
2023-03-03 13:01:05 -0800

    save

--- query_data.py
+++ query_data.py
@@ -8,14 +8,27 @@ from langchain.chains.llm import LLMChain
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.vectorstores.base import VectorStore
+from pydantic import Field
 
+doc_template="""---
+href: {href}
+section: {section}
+content:
+{page_content}
+---
+"""
+
+QUARTO_DOC_PROMPT = PromptTemplate(
+    template=doc_template,
+    input_variables=["page_content", "href", "section"]
+)
 
 prompt_template = """You are an AI assistant for the open source library Quarto. The documentation is located at https://quarto.org/docs
 You are given the following extracted parts of a long document and a question. Provide a conversational answer with a hyperlink to the documentation.
 You should only use hyperlinks that are explicitly listed as a source in the context. Do NOT make up a hyperlink that is not listed.
 If the question includes a request for code, provide a code block directly from the documentation.
 If you don't know the answer, just say "Hmm, I'm not sure." Don't try to make up an answer.
-If the question is not about LangChain, politely inform them that you are tuned to only answer questions about LangChain.
+If the question is not about Quarto, politely inform them that you are tuned to only answer questions about Quarto.
 Question: {question}
 =========
 {context}
@@ -58,7 +71,8 @@ def get_chain(
         llm=question_gen_llm, prompt=CONDENSE_QUESTION_PROMPT, callback_manager=manager
     )
     doc_chain = load_qa_chain(
-        streaming_llm, chain_type="stuff", prompt=QA_PROMPT, callback_manager=manager
+        streaming_llm, chain_type="stuff", prompt=QA_PROMPT, document_prompt=QUARTO_DOC_PROMPT,
+        callback_manager=manager
     )
 
     qa = ChatVectorDBChain(
