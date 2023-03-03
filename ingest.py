e418396c23f3ba71d3d4ebc561ee63334f611174
Hamel Husain <hamel.husain@gmail.com>
2023-03-03 10:06:00 -0800

    save

--- ingest.py
+++ ingest.py
@@ -1,15 +1,36 @@
 """Load html from files, clean up, split, ingest into Weaviate."""
 import pickle
-
-from langchain.document_loaders import ReadTheDocsLoader
+import json
+from langchain.docstore.document import Document
+from langchain.document_loaders.base import BaseLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
+from typing import List
+
+
+class QuartoLoader(BaseLoader):
+    """Load Quarto search.json files."""
+
+    def __init__(self, file_path: str):
+        """Initialize with file path."""
+        self.file_path = file_path
+
+    def load(self) -> List[Document]:
+        """Load json from file path."""
+        with open(self.file_path) as f:
+            index = json.loads(f.read())
+        
+        docs = []
+        for doc in index:
+            metadata = {k: doc[k] for k in ("objectID", "href", "section")}
+            docs.append(Document(page_content=doc["text"], metadata=metadata))
+        return docs
 
 
 def ingest_docs():
     """Get documents from web pages."""
-    loader = ReadTheDocsLoader("langchain.readthedocs.io/en/latest/")
+    loader = QuartoLoader("search.json")
     raw_documents = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
--- query_data.py
+++ query_data.py
@@ -2,12 +2,28 @@
 from langchain.callbacks.base import AsyncCallbackManager
 from langchain.callbacks.tracers import LangChainTracer
 from langchain.chains import ChatVectorDBChain
-from langchain.chains.chat_vector_db.prompts import (CONDENSE_QUESTION_PROMPT,
-                                                     QA_PROMPT)
+from langchain.chains.chat_vector_db.prompts import CONDENSE_QUESTION_PROMPT
+from langchain.prompts.prompt import PromptTemplate
 from langchain.chains.llm import LLMChain
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.vectorstores.base import VectorStore
+
+
+prompt_template = """You are an AI assistant for the open source library Quarto. The documentation is located at https://quarto.org/docs
+You are given the following extracted parts of a long document and a question. Provide a conversational answer with a hyperlink to the documentation.
+You should only use hyperlinks that are explicitly listed as a source in the context. Do NOT make up a hyperlink that is not listed.
+If the question includes a request for code, provide a code block directly from the documentation.
+If you don't know the answer, just say "Hmm, I'm not sure." Don't try to make up an answer.
+If the question is not about LangChain, politely inform them that you are tuned to only answer questions about LangChain.
+Question: {question}
+=========
+{context}
+=========
+Answer in Markdown:"""
+QA_PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
 
 
 def get_chain(
