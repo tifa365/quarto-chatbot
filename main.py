477b7b89f453f6fb9e234394103a7e85c9d39257
Hamel Husain <hamel.husain@gmail.com>
2023-03-03 16:00:00 -0800

    edit template to accomodate markdown

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from typing import Optional
 
 from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from langchain.vectorstores import VectorStore
 
@@ -13,6 +14,7 @@ from query_data import get_chain
 from schemas import ChatResponse
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 vectorstore: Optional[VectorStore] = None
 
